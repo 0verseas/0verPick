5bad1b5cd1ef4fe14cfa8d33c4bd6096d08c8ab9
YY <z3388638@gmail.com>
2017-11-24 02:34:07 +0800

    add login.js

---
+++ src/js/login.js
@@ -0,0 +1,21 @@
+(() => {
+	/**
+	 * cache dom
+	 */
+	const $loginForm = $('.LoginForm');
+
+	/**
+	 * bind event
+	 */
+	$loginForm.on('submit', _handleLogin);
+
+	/**
+	 * event handler
+	 */
+	function _handleLogin(e) {
+		const username = $loginForm.find('.LoginForm__input-username').val();
+		const password = $loginForm.find('.LoginForm__input-password').val();
+		console.log([username, password]);
+		e.preventDefault();
+	}
+})();
