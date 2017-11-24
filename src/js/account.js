f328fba3d37fcb6160ab216694949b252e85b999
YY <z3388638@gmail.com>
2017-11-24 12:26:40 +0800

    選取可查看系所

--- src/js/account.js
+++ src/js/account.js
@@ -4,12 +4,16 @@
 	 */
 	const $AccountModal = $('.AccountModal');
 	const $AccountList = $('.AccountList');
+	const $DeptList = $('.DeptList');
+	const $SelectedDeptList = $('.SelectedDeptList');
 
 	/**
 	 * bind event
 	 */
 	$AccountModal.on('show.bs.modal', _handleShowAccountModal);
 	$AccountList.on('click.delAccount', '.AccountItem__btn-del', _handleDelAccount);
+	$DeptList.on('click.select', '.DeptList__item .btn-select', _handleSelectDept);
+	$SelectedDeptList.on('click.select', '.SelectedDeptList__item .btn-remove', _handleremoveDept);
 
 	/**
 	 * event handler
@@ -25,6 +29,40 @@
 
 	function _handleDelAccount() {
 		alert();
+	}
+
+	function _handleSelectDept() {
+		const $oriItem = $(this).parents('.DeptList__item')
+		const title = $oriItem.find('.title').text();
+		const $newItem = $(`
+			<div class="pb-1 pl-1 pr-1 SelectedDeptList__item">
+				<span class="title">${title}</span>
+				<span class="btn-remove">
+					<i class="fa fa-arrow-left d-none d-lg-inline" aria-hidden="true"></i>
+					<i class="fa fa-arrow-up d-inline d-lg-none" aria-hidden="true"></i>
+				</span>
+			</div>
+		`);
+
+		$oriItem.remove();
+		$SelectedDeptList.append($newItem);
+	}
+
+	function _handleremoveDept() {
+		const $oriItem = $(this).parents('.SelectedDeptList__item')
+		const title = $oriItem.find('.title').text();
+		const $newItem = $(`
+			<div class="pb-1 pl-1 pr-1 DeptList__item">
+				<span class="title">${title}</span>
+				<span class="btn-select">
+					<i class="fa fa-arrow-right d-none d-lg-inline" aria-hidden="true"></i>
+					<i class="fa fa-arrow-down d-inline d-lg-none" aria-hidden="true"></i>
+				</span>
+			</div>
+		`);
+
+		$oriItem.remove();
+		$DeptList.append($newItem);
 	}
 
 	/**
