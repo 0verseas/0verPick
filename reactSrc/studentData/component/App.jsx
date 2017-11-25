8271e6bb286d1bb77531958fde6b48fd596f021b
YY <z3388638@gmail.com>
2017-11-26 02:31:41 +0800

    layout and some logic for filtering student data

--- reactSrc/studentData/component/App.jsx
+++ reactSrc/studentData/component/App.jsx
@@ -25,13 +25,13 @@ export default class App extends React.Component {
 		const systemName = ['', '學士班', '港二技', '碩士班', '博士班'];
 		return (
 			<div>
-				<Row>
+				<Row className="mb-2">
 					<Col>
 						<h4 className="d-inline">{ `${systemName[this._systemID]}學生資料` }</h4>
 						<Button className="aligb-top float-right" color="primary" size="sm">下載學生基本資料(Excel)</Button>
 					</Col>
 				</Row>
-				<Row>
+				<Row className="mb-2">
 					<Col>
 						<StudentDataFilter />
 					</Col>
--- reactSrc/studentData/component/StudentDataFilter.jsx
+++ reactSrc/studentData/component/StudentDataFilter.jsx
@@ -1,9 +1,120 @@
 import React from 'react';
+import {
+	Form,
+	FormGroup,
+	Col,
+	Input,
+	Label,
+	Button
+} from 'reactstrap';
 
 export default class StudentDataFilter extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			open: false,
+			no: '',
+			name: '',
+			email: '',
+			resident: '',
+			order: '',
+			school: '',
+			dept: ''
+		};
+
+		this.handleToggle = this.handleToggle.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
+		this.handleReset = this.handleReset.bind(this);
+	}
+
+	handleToggle() {
+		this.setState((prevState) => ({
+			open: !prevState.open
+		}));
+	}
+
+	handleSubmit(e) {
+		console.log('lalalal');
+		e && e.preventDefault();
+	}
+
+	handleReset() {
+		this.setState({
+			no: '',
+			name: '',
+			email: '',
+			resident: '',
+			order: '',
+			school: '',
+			dept: ''
+		}, this.handleSubmit);
+	}
+
 	render() {
 		return (
-			<div>&lt;Filter/&gt;</div>
+			<div>
+				<div className="mb-2 pl-3" style={{background: '#666', color: '#f3f3f3', fontSize: '16px', cursor: 'pointer'}} onClick={this.handleToggle}>
+					{
+						this.state.open ? (<i className="fa fa-caret-down" aria-hidden="true"></i>)
+						: (<i className="fa fa-caret-right" aria-hidden="true"></i>)
+					}
+
+					{' '}過濾條件
+				</div>
+
+				{ this.state.open &&
+					<Form onSubmit={this.handleSubmit}>
+						<FormGroup row>
+							<Label for="no" sm={2}>僑生/港澳生編號</Label>
+							<Col sm={10}>
+								<Input type="text" name="no" id="no" value={this.state.no} onChange={(e) => { this.setState({ no: e.target.value })}} />
+							</Col>
+						</FormGroup>
+						<FormGroup row>
+							<Label for="name" sm={2}>姓名</Label>
+							<Col sm={10}>
+								<Input type="text" name="name" id="name" value={this.state.name} onChange={(e) => { this.setState({ name: e.target.value })}} />
+							</Col>
+						</FormGroup>
+						<FormGroup row>
+							<Label for="email" sm={2}>Email</Label>
+							<Col sm={10}>
+								<Input type="email" name="email" id="email" value={this.state.email} onChange={(e) => { this.setState({ email: e.target.value })}} />
+							</Col>
+						</FormGroup>
+						<FormGroup row>
+							<Label for="resident" sm={2}>僑居地</Label>
+							<Col sm={10}>
+								<Input type="text" name="resident" id="resident" value={this.state.resident} onChange={(e) => { this.setState({ resident: e.target.value })}} />
+							</Col>
+						</FormGroup>
+						<FormGroup row>
+							<Label for="order" sm={2}>志願序</Label>
+							<Col sm={10}>
+								<Input type="text" name="order" id="order" value={this.state.order} onChange={(e) => { this.setState({ order: e.target.value })}} />
+							</Col>
+						</FormGroup>
+						<FormGroup row>
+							<Label for="school" sm={2}>畢業學校</Label>
+							<Col sm={10}>
+								<Input type="text" name="school" id="school" value={this.state.school} onChange={(e) => { this.setState({ school: e.target.value })}} />
+							</Col>
+						</FormGroup>
+						<FormGroup row>
+							<Label for="dept" sm={2}>申請系所名稱</Label>
+							<Col sm={10}>
+								<Input type="text" name="dept" id="dept" value={this.state.dept} onChange={(e) => { this.setState({ dept: e.target.value })}} />
+							</Col>
+						</FormGroup>
+						<FormGroup check row>
+							<Col sm={{ size: 10, offset: 2 }}>
+								<Button color="primary">查詢</Button>{' '}
+								<Button type="button" color="secondary" onClick={this.handleReset}>清除所有條件</Button>
+							</Col>
+						</FormGroup>
+					</Form>
+				}
+			</div>
 		);
 	}
 }
