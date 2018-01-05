f207629065f0b6ad7fa642de926426874397b68d
hewen-lotus <lotus3698@gmail.com>
2018-01-05 11:54:07 +0800

    首頁放上系代碼對照連結

--- src/js/index.js
+++ src/js/index.js
@@ -5,6 +5,7 @@
 	 */
 	const $downloadPtt = $('#downloadPtt');
 	const $importAccount = $('#importAccount');
+	const $deptID = $('#deptID');
 	const _config = window.getConfig();
 
 
@@ -27,5 +28,25 @@
 		</div>
 		`;
 	$importAccount.html(importAccountHTML);
+
+	let deptIDHTML='';
+	deptIDHTML += `
+		<div>
+			<a href="${_config.apiBase}/forms/學士系代碼對照表.ods"  target="_blank">
+				<i class="fa fa-download" aria-hidden="true"></i> 學士班系代碼代碼表
+			</a>
+			<a href="${_config.apiBase}/forms/港二技代碼對照表.ods"  target="_blank">
+				<i class="fa fa-download" aria-hidden="true"></i> 港二技代碼對照表
+			</a>
+			<a href="${_config.apiBase}/forms/碩士系代碼對照表.ods"  target="_blank">
+				<i class="fa fa-download" aria-hidden="true"></i> 碩士班系代碼代碼表
+			</a>
+			<a href="${_config.apiBase}/forms/博士系代碼對照表.ods"  target="_blank">
+				<i class="fa fa-download" aria-hidden="true"></i> 博士班系代碼代碼表
+			</a>
+			<span style="font-size: 15px;">（匯入帳號的各學制權限請使用以上提供的系代碼）</span>
+		</div>
+		`;
+    $deptID.html(deptIDHTML);
 
 })();
