0355991728cd92363c78f9ef628d305980af4aa3
lemon5920 <lemon5920@gmail.com>
2018-01-20 01:37:20 +0800

    Merge

--- reactSrc/studentData/component/StudentDataTable.jsx
+++ reactSrc/studentData/component/StudentDataTable.jsx
@@ -147,8 +147,8 @@ export default class StudentDataTable extends React.Component {
 											<td>{val.resident}</td>
 											<td>{val.order}</td>
 											<td className="text-center"><Button color="primary" size="sm" onClick={() => { this.props.onDetail(val.userID, val.deptID) }}>學生詳細資料</Button></td>
-											<td className="text-center"><Button color="success" size="sm" onClick={() => { this.getStudentMergedFile(val.userID, val.deptID, val.no, val.name, val.dept, 'pdf') } }>下載審查資料（PDF 檔）</Button></td>
-											<td className="text-center"><Button color="success" size="sm" onClick={() => { this.getStudentMergedFile(val.userID, val.deptID, val.no, val.name, val.dept, 'raw') } }>下載審查資料（原始檔）</Button></td>
+											<td className="text-center"><Button color="success" size="sm" onClick={() => { this.getStudentMergedFile(val.userID, val.deptID, 'pdf') } }>下載審查資料（PDF 檔）</Button></td>
+											<td className="text-center"><Button color="success" size="sm" onClick={() => { this.getStudentMergedFile(val.userID, val.deptID, 'raw') } }>下載審查資料（原始檔）</Button></td>
 										</tr>
 									);
 								})
