aafeeae5e61cc48caa228c9cd97c731d7c70e8fb
richegg <zxp86021@gmail.com>
2018-12-25 13:54:39 +0800

    enable 按鈕

--- src/js/download.js
+++ src/js/download.js
@@ -54,13 +54,13 @@ const app = ( () => {
 				<tr>
 					<td>${dept.title}</td>
 					<td class="text-center btn-download">
-						<button class="btn btn-success btn-sm" onclick="app.getStudentApplicationDocsFile('${system}', '${dept.id}', 'pdf')">
+						<button class="btn btn-success btn-sm" onclick="app.getStudentApplicationDocsFile('${system}', '${dept.id}', 'pdf');">
 							<i class="fa fa-download" aria-hidden="true"></i> PDF 合併檔
 						</button>
 					</td>
 					<!--
 					<td class="text-center btn-download">
-						<button class="btn btn-success btn-sm" onclick="app.getStudentApplicationDocsFile('${system}', '${dept.id}', 'raw')">
+						<button class="btn btn-success btn-sm" onclick="app.getStudentApplicationDocsFile('${system}', '${dept.id}', 'raw');">
 							<i class="fa fa-download" aria-hidden="true"></i> 原始檔案合併檔
 						</button>
 					</td>
