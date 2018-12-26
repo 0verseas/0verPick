88c5c9c08977437dd7c79710bebc7ca207b2e950
richegg <zxp86021@gmail.com>
2018-12-26 16:12:48 +0800

    修正全校下載 zip 按鈕的顯示

--- src/js/download.js
+++ src/js/download.js
@@ -11,6 +11,11 @@ const app = ( () => {
 	const $masterTbody = $('#masterTbody');
 	const $phdTbody = $('#phdTbody');
 
+	const $allpdfzip = $('#allpdfzip');
+	const $twoyearallpdfzip = $('#twoyearallpdfzip');
+
+	let _two_year_tech_depts = [];
+
 	/**
 	 * init
 	 */
@@ -41,8 +46,24 @@ const app = ( () => {
 			_renderDepartments($twoYearTechTbody, 'two-year', '港二技', data.two_year_tech_depts)
 			_renderDepartments($masterTbody, 'master', '碩士班', data.master_depts)
 			_renderDepartments($phdTbody, 'phd', '博士班', data.phd_depts)
+
+			_two_year_tech_depts = data.two_year_tech_depts;
 		});
 
+		PubSub.on('user', (user) => {
+			if (!!user.school_reviewer.has_admin) {
+				$allpdfzip.show();
+
+				if (_two_year_tech_depts.length < 1) {
+					$twoyearallpdfzip.remove();
+				} else {
+					$twoyearallpdfzip.show();
+				}
+			} else {
+				$twoyearallpdfzip.remove();
+				$allpdfzip.remove();
+			}
+		});
 	}
 
 	// 渲染某學制系所
@@ -58,13 +79,6 @@ const app = ( () => {
 							<i class="fa fa-download" aria-hidden="true"></i> PDF 合併檔
 						</button>
 					</td>
-					<!--
-					<td class="text-center btn-download">
-						<button class="btn btn-success btn-sm" onclick="app.getStudentApplicationDocsFile('${system}', '${dept.id}', 'raw');">
-							<i class="fa fa-download" aria-hidden="true"></i> 原始檔案合併檔
-						</button>
-					</td>
-					-->
 				</tr>
 			`;
 		}
