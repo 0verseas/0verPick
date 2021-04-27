2e7c29a62a677f679d28c8f7e3dc080ec0fc5e87
Rick <ricklai82@gmail.com>
2021-04-27 10:27:18 +0800

    個申審核系統Login導入reCAPTCHA。

--- src/js/login.js
+++ src/js/login.js
@@ -4,6 +4,12 @@
 	 */
 	const _config = window.getConfig();
 
+	// 引入 reCAPTCHA 的 JS 檔案
+    var s = document.createElement('script');
+    let src = 'https://www.google.com/recaptcha/api.js?render=' + _config.reCAPTCHA_site_key;
+    s.setAttribute('src', src);
+    document.body.appendChild(s);
+
 	/**
 	 * cache dom
 	 */
@@ -40,26 +46,45 @@
 		
 		const username = $loginForm.find('.LoginForm__input-username').val();
 		const password = $loginForm.find('.LoginForm__input-password').val();
-		window.API.login({
-			username,
-			password: sha256(password)
-		}, function (err, data) {
-			if (err) {
-				console.error(err);
-				if (err.status === 401) {
-					alert('帳號或密碼有誤');
-				}
-				
-				return;
-			}
 
-			const url = window.location.href.split('?url=')[1];
-			if (url) {
-				window.location.href = url;
-			} else {
-				window.location.href = './';
-			}
-		});
+		var loginForm = {
+			username: username,
+			password: sha256(password),
+			google_recaptcha_token: ''
+		}
+
+		grecaptcha.ready(function() {
+            grecaptcha.execute(_config.reCAPTCHA_site_key, {
+              action: 'PickLogin'
+            }).then(function(token) {
+                // token = document.getElementById('btn-login').value
+                loginForm.google_recaptcha_token=token;
+            }).then(function(){
+				window.API.login(loginForm, function (err, data) {
+					if (err) {
+						// console.error(err);
+
+						if (err.status == 401) {
+							alert('帳號或密碼有誤');
+						} else if (err.status == 403) {
+							alert('Google reCAPTCHA verification failed');
+						}
+						else if (err.status == 429) {  // 429 Too Many Requests
+							alert('錯誤次數太多，請稍後再試。');
+						}
+						
+						return;
+					}
+		
+					const url = window.location.href.split('?url=')[1];
+					if (url) {
+						window.location.href = url;
+					} else {
+						window.location.href = './';
+					}
+				});
+			});
+        });
 
 		e.preventDefault();
 	}
